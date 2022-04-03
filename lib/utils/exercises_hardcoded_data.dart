b950bd00e9152a7c5d9a9c2d6d719e7ad3bb191d
Andrew Villegas <villegas.abva@gmail.com>
2022-04-03 13:43:32 -0500

    feat: hardcode exercises

---
+++ lib/utils/exercises_hardcoded_data.dart
@@ -0,0 +1,373 @@
+class ExercisesHardcodedData {
+  List<Map<String, dynamic>> exercisesMap = [
+    // Lunes
+    {
+      'day': 'Lunes',
+      'muscles targeted': ['Cuadriceps', 'Pantorrilla', 'Abs'],
+      'exercises': [
+        {
+          'Cuadriceps': [
+            {
+              'exercise': 'Extensiones',
+              'sets': 4,
+              'reps': 20,
+              'rest': '30 segundos'
+            },
+            {
+              'exercise': 'Sentadilla Haka',
+              'sets': 4,
+              'reps': 20,
+              'rest': '30 segundos'
+            },
+            {
+              'exercise': 'Prensa tijeras caminando (Zancadas)',
+              'sets': 4,
+              'reps': 20,
+              'rest': '30 segundos'
+            },
+          ],
+        },
+        {
+          'Pantorrilla': [
+            {
+              'exercise': 'Sentado (Soleo)',
+              'sets': 4,
+              'reps': 20,
+              'rest': '30 segundos'
+            },
+            {
+              'exercise': 'En maquina de pie o prensa',
+              'sets': 4,
+              'reps': 20,
+              'rest': '30 segundos'
+            },
+          ]
+        },
+        {
+          'Abdomen': [
+            {
+              'exercise':
+                  'Recto abdominal en colchoneta con  piernas cruzadas en el aire',
+              'sets': 4,
+              'reps': 50,
+              'rest': '30 segundos'
+            },
+            {
+              'exercise': 'Elevacion de piernas estiradas (en colchoneta)',
+              'sets': 4,
+              'reps': 'all fallo',
+              'rest': '30 segundos'
+            },
+          ]
+        },
+      ]
+    },
+
+    // Martes
+    {
+      'day': 'Martes',
+      'muscles': ['Pecho', 'Triceps', 'Abs'],
+      'exercises': [
+        {
+          'Pecho': [
+            {
+              'exercise': 'Press plano con barra',
+              'sets': 4,
+              'reps': 20,
+              'rest': '30 segundos'
+            },
+            {
+              'exercise': 'Press inclinado multipower o con barra',
+              'sets': 4,
+              'reps': 20,
+              'rest': '30 segundos'
+            },
+            {
+              'exercise': 'Press plano con mancuernas',
+              'sets': 4,
+              'reps': 20,
+              'rest': '30 segundos'
+            },
+            {
+              'exercise': 'Pullover con mancuerna',
+              'sets': 4,
+              'reps': 20,
+              'rest': '30 segundos'
+            },
+          ],
+        },
+        {
+          'Triceps': [
+            {
+              'exercise': 'Polea para triceps con cuerda',
+              'sets': 4,
+              'reps': 20,
+              'rest': '30 segundos'
+            },
+            {
+              'exercise': 'Press frances con barra Z',
+              'sets': 4,
+              'reps': 20,
+              'rest': '30 segundos'
+            },
+            {
+              'exercise': 'Press frances con mancuerna',
+              'sets': 4,
+              'reps': 20,
+              'rest': '30 segundos'
+            },
+            {'exercise': 'Copa', 'sets': 4, 'reps': 20, 'rest': '30 segundos'},
+          ]
+        },
+        {
+          'Abdomen': [
+            {
+              'exercise':
+                  'Recto abdominal en colchoneta con  piernas cruzadas en el aire',
+              'sets': 4,
+              'reps': 50,
+              'rest': '30 segundos'
+            },
+            {
+              'exercise': 'Elevacion de piernas estiradas (en colchoneta)',
+              'sets': 4,
+              'reps': 'all fallo',
+              'rest': '30 segundos'
+            },
+          ]
+        },
+      ]
+    },
+
+    // Miercoles
+    {
+      'day': 'Miercoles',
+      'muscles': ['Espalda', 'Biceps', 'Antebrazo', 'Abs'],
+      'exercises': [
+        {
+          'Espalda': [
+            {
+              'exercise': 'Remo bajo en polea con agarre amplio',
+              'sets': 4,
+              'reps': 20,
+              'rest': '30 segundos'
+            },
+            {
+              'exercise': 'Jalon al pecho con agarre amplio',
+              'sets': 4,
+              'reps': 20,
+              'rest': '30 segundos'
+            },
+            {
+              'exercise': 'Remo mancuerna a 1 mano',
+              'sets': 4,
+              'reps': 20,
+              'rest': '30 segundos'
+            },
+            {
+              'exercise': 'Remo barra agarre prono',
+              'sets': 4,
+              'reps': 20,
+              'rest': '30 segundos'
+            },
+          ],
+        },
+        {
+          'Biceps': [
+            {
+              'exercise': 'Curl barra recta de pie',
+              'sets': 4,
+              'reps': 20,
+              'rest': '30 segundos'
+            },
+            {
+              'exercise': 'Curl predicador barra Z',
+              'sets': 4,
+              'reps': 20,
+              'rest': '30 segundos'
+            },
+            {
+              'exercise': 'Alterno mancuernas sentado',
+              'sets': 4,
+              'reps': 20,
+              'rest': '30 segundos'
+            },
+          ]
+        },
+        {
+          'Abdomen': [
+            {
+              'exercise':
+                  'Recto abdominal en colchoneta con  piernas cruzadas en el aire',
+              'sets': 4,
+              'reps': 50,
+              'rest': '30 segundos'
+            },
+            {
+              'exercise': 'Elevacion de piernas estiradas (en colchoneta)',
+              'sets': 4,
+              'reps': 'all fallo',
+              'rest': '30 segundos'
+            },
+          ]
+        },
+      ]
+    },
+
+    // Jueves
+    {
+      'day': 'Jueves',
+      'muscles': [
+        'Hombros',
+        'Trapecio',
+        'Abs',
+      ],
+      'exercises': [
+        {
+          'Hombros': [
+            {
+              'exercise': 'Elevacion lateral con mancuerna sentado',
+              'sets': 4,
+              'reps': 20,
+              'rest': '30 segundos'
+            },
+            {
+              'exercise': 'Press mancuerna',
+              'sets': 4,
+              'reps': 20,
+              'rest': '30 segundos'
+            },
+            {
+              'exercise': 'Elevacion lateral con cable a 1 mano',
+              'sets': 4,
+              'reps': 20,
+              'rest': '30 segundos'
+            },
+            {
+              'exercise': 'Elevacion frontal con mancuerna sentado',
+              'sets': 4,
+              'reps': 20,
+              'rest': '30 segundos'
+            },
+          ],
+        },
+        {
+          'Abdomen': [
+            {
+              'exercise':
+                  'Recto abdominal en colchoneta con  piernas cruzadas en el aire',
+              'sets': 4,
+              'reps': 50,
+              'rest': '30 segundos'
+            },
+            {
+              'exercise': 'Elevacion de piernas estiradas (en colchoneta)',
+              'sets': 4,
+              'reps': 'all fallo',
+              'rest': '30 segundos'
+            },
+          ]
+        },
+      ]
+    },
+
+    // Viernes
+    {
+      'day': 'Viernes',
+      'muscles': ['Femoral', 'Pantorrilla', 'Gluteos', 'Abs'],
+      'exercises': [
+        {
+          'Femoral': [
+            {
+              'exercise': 'Femoral maquina',
+              'sets': 4,
+              'reps': 20,
+              'rest': '30 segundos'
+            },
+            {
+              'exercise': 'Femoral sentado o peso muerto con mancuerna',
+              'sets': 4,
+              'reps': 20,
+              'rest': '30 segundos'
+            },
+            {
+              'exercise': 'Camilla de femoral con piernas juntas con mancuerna',
+              'sets': 4,
+              'reps': 20,
+              'rest': '30 segundos'
+            },
+          ],
+        },
+        {
+          'Pantorrilla': [
+            {
+              'exercise': 'Sentado (Soleo)',
+              'sets': 4,
+              'reps': 20,
+              'rest': '30 segundos'
+            },
+            {
+              'exercise': 'En maquina de pie o prensa',
+              'sets': 4,
+              'reps': 20,
+              'rest': '30 segundos'
+            },
+          ]
+        },
+        {
+          'Gluteos': [
+            {
+              'exercise': 'Hip thrust',
+              'sets': 4,
+              'reps': 20,
+              'rest': '30 segundos'
+            },
+            {
+              'exercise': 'Patada para gluteos',
+              'sets': 4,
+              'reps': 20,
+              'rest': '30 segundos'
+            },
+            {
+              'exercise': 'Tijeras caminando',
+              'sets': 4,
+              'reps': 20,
+              'rest': '30 segundos'
+            },
+            {
+              'exercise': 'Maquina sentado abduccion',
+              'sets': 4,
+              'reps': 20,
+              'rest': '30 segundos'
+            },
+          ],
+        },
+        {
+          'Abdomen': [
+            {
+              'exercise':
+                  'Recto abdominal en colchoneta con  piernas cruzadas en el aire',
+              'sets': 4,
+              'reps': 50,
+              'rest': '30 segundos'
+            },
+            {
+              'exercise': 'Elevacion de piernas estiradas (en colchoneta)',
+              'sets': 4,
+              'reps': 'all fallo',
+              'rest': '30 segundos'
+            },
+          ]
+        },
+      ]
+    },
+    {
+      'day': 'Sabado',
+      'muscles': ['descanso']
+    },
+    {
+      'day': 'Domingo',
+      'muscles': ['descanso']
+    }
+  ];
+}
