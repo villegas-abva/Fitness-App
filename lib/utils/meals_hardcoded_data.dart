08eb311cf82731e140c006eee9c3001de87af3a6
Andrew Villegas <villegas.abva@gmail.com>
2022-04-05 04:46:30 -0500

    feat: hardcode meals

---
+++ lib/utils/meals_hardcoded_data.dart
@@ -0,0 +1,70 @@
+class ExercisesHardcodedData {
+  List<Map<String, dynamic>> meals = [
+    // Comida 1
+    {
+      'meal': 'comida 1',
+      'elements': ['50 g avena', '2 frutillas', '4 claras de huevo']
+    },
+
+    // Comida 2
+    {
+      'meal': 'comida 2',
+      'ingredients': [
+        '80 g arroz',
+        '100 g pollo',
+        '50 g vegetales verdes',
+        '1/4 de aguacate'
+      ]
+    },
+
+    // Comida 3
+    {
+      'meal': 'comida 3',
+      'ingredients': [
+        '80 g papa',
+        '100 g carne',
+        '50 g vegetales',
+        '4 almendras'
+      ]
+    },
+
+    // Comida 4
+    {
+      'meal': 'comida 4',
+      'ingredients': [
+        '80 g camote',
+        '100 g pescado',
+        '50 g aguacate o 1 cucharada de aceite de oliva'
+      ]
+    },
+
+    // Comida 5
+    {
+      'meal': 'comida 5',
+      'ingredients': ['100 g pollo', '80 g arroz', '50 g vetegetales verdes']
+    },
+
+    // Opcional
+    {
+      'meal': 'opcional',
+      'ingredients': [
+        '6 claras de huevo',
+        'Omelette de cebolla y tomate',
+        '1 te verde',
+        '50 g pescado',
+        '50 g aguacate o 1 cucharada de aceite de oliva'
+      ]
+    },
+
+    {
+      'meal': 'opcional',
+      'ingredients': [
+        '1 guineo',
+        'Avena',
+        'Canela',
+        'Proteina en polvo',
+        'Agua'
+      ]
+    },
+  ];
+}
