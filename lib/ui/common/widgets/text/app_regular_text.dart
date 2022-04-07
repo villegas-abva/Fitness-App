eea2d8fea239bf4c8bce8d45b7d76b0830f010c4
Andrew Villegas <villegas.abva@gmail.com>
2022-04-06 20:38:20 -0500

    feat: center title to app regular text

--- lib/ui/common/widgets/text/app_regular_text.dart
+++ lib/ui/common/widgets/text/app_regular_text.dart
@@ -5,6 +5,7 @@ class AppRegularText extends StatelessWidget {
   final String text;
   final Color color;
   final bool isBold;
+  final bool isCentered;
   final bool hasOverflow;
   final int maxLines;
 
@@ -14,6 +15,7 @@ class AppRegularText extends StatelessWidget {
       this.color = Colors.black54,
       this.size = 16,
       this.isBold = false,
+      this.isCentered = false,
       this.hasOverflow = false,
       this.maxLines = 10})
       : super(key: key);
@@ -23,6 +25,8 @@ class AppRegularText extends StatelessWidget {
     return Text(
       text,
       maxLines: maxLines,
+      textAlign: TextAlign.center,
+      // textAlign: isCentered ? TextAlign.center : TextAlign.left,
       overflow: TextOverflow.ellipsis,
       style: TextStyle(
           color: color,
