101020ed174b030c16560b5a11e76c783ba9d041
Andrew Villegas <villegas.abva@gmail.com>
2022-04-09 09:49:24 -0500

    feat: rest days display lottie animation

--- lib/ui/screens/exercises/exercises_screen.dart
+++ lib/ui/screens/exercises/exercises_screen.dart
@@ -2,6 +2,7 @@ import 'package:fit_app/ui/common/widgets/text/app_large_text.dart';
 import 'package:fit_app/ui/common/widgets/text/app_regular_text.dart';
 import 'package:fit_app/utils/exercises_hardcoded_data.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class ExercisesScreen extends StatefulWidget {
   // final int selectedIndex;
@@ -94,106 +95,139 @@ class _ExercisesScreenState extends State<ExercisesScreen> {
         physics: const NeverScrollableScrollPhysics(),
         itemCount: day['muscles'].length,
         itemBuilder: (context, muscleIndex) {
-          return Padding(
-            padding: const EdgeInsets.only(
-              left: 15,
-              bottom: 30,
-            ),
-            child: Column(
+          if (day['day'] == 'Sabado' || day['day'] == 'Domingo') {
+            // return Column(
+            //   children: [
+            //     AppLargeText(
+            //         size: 22,
+            //         text: day['muscles'][muscleIndex]['muscle'].toString(),
+            //         color: Colors.white),
+            //     AppLargeText(size: 22, text: 'hula', color: Colors.white),
+            //   ],
+            // );
+            return Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                // Muscle
-                AppLargeText(
-                    size: 22,
-                    text: day['muscles'][muscleIndex]['muscle'].toString(),
-                    color: Colors.white),
-                const SizedBox(height: 10),
-                SizedBox(
-                  height: 200,
-                  width: double.infinity,
-                  child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      shrinkWrap: true,
-                      itemCount:
-                          day['muscles'][muscleIndex]['exercises'].length,
-                      itemBuilder: (context, exerciseIndex) {
-                        return Padding(
-                          padding: const EdgeInsets.only(right: 12.0),
-
-                          // Image
-                          child: Stack(
-                            children: [
-                              Container(
-                                decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                    fit: BoxFit.cover,
-                                    colorFilter: ColorFilter.mode(
-                                        Colors.black.withOpacity(0.5),
-                                        BlendMode.dstATop),
-                                    image: AssetImage(day['muscles']
-                                            [muscleIndex]['exercises']
-                                        [exerciseIndex]['image']),
-                                  ),
-                                  color: Colors.white10,
-                                  borderRadius: BorderRadius.circular(20),
-                                ),
-                                padding: const EdgeInsets.all(10),
-                                width: 280,
-
-                                // Exercise
-                                child: Center(
-                                  child: AppRegularText(
-                                      text: day['muscles'][muscleIndex]
-                                              ['exercises'][exerciseIndex]
-                                          ['exercise'],
-                                      color: Colors.white,
-                                      size: 22),
-                                ),
-                              ),
-                              Positioned(
-                                top: 10,
-                                right: 10,
-                                child: Container(
-                                  padding: const EdgeInsets.all(5),
-                                  decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(20),
-                                      color: Colors.orange),
-                                  child: AppRegularText(
-                                      text:
-                                          '${day['muscles'][muscleIndex]['exercises'][exerciseIndex]['sets']} x ${day['muscles'][muscleIndex]['exercises'][exerciseIndex]['reps']} reps',
-                                      color: Colors.black,
-                                      size: 13),
-                                ),
-                              ),
-                              Positioned(
-                                bottom: 10,
-                                right: 10,
-                                child: Container(
-                                  padding: const EdgeInsets.all(5),
-                                  decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(20),
-                                      color: Colors.orange),
-                                  child: Row(
-                                    children: [
-                                      const Icon(Icons.watch),
-                                      const SizedBox(width: 5),
-                                      AppRegularText(
-                                          text:
-                                              '${day['muscles'][muscleIndex]['exercises'][exerciseIndex]['rest']} rest',
-                                          color: Colors.black,
-                                          size: 13),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        );
-                      }),
+                Center(
+                  child: Lottie.asset('assets/lottie_animations/baby.json',
+                      height: 220, width: 220),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  height: 70,
+                  margin: EdgeInsets.symmetric(horizontal: 20),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: Colors.white),
+                  child: Center(
+                    child: AppRegularText(
+                        size: 22, text: 'Dia de Descanso', color: Colors.black),
+                  ),
                 ),
               ],
-            ),
-          );
+            );
+          } else {
+            return Padding(
+              padding: const EdgeInsets.only(
+                left: 15,
+                bottom: 30,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Muscle
+                  AppLargeText(
+                      size: 22,
+                      text: day['muscles'][muscleIndex]['muscle'].toString(),
+                      color: Colors.white),
+                  const SizedBox(height: 10),
+                  SizedBox(
+                    height: 200,
+                    width: double.infinity,
+                    child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        shrinkWrap: true,
+                        itemCount:
+                            day['muscles'][muscleIndex]['exercises'].length,
+                        itemBuilder: (context, exerciseIndex) {
+                          return Padding(
+                            padding: const EdgeInsets.only(right: 12.0),
+
+                            // Image
+                            child: Stack(
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      fit: BoxFit.cover,
+                                      colorFilter: ColorFilter.mode(
+                                          Colors.black.withOpacity(0.5),
+                                          BlendMode.dstATop),
+                                      image: AssetImage(day['muscles']
+                                              [muscleIndex]['exercises']
+                                          [exerciseIndex]['image']),
+                                    ),
+                                    color: Colors.white10,
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  padding: const EdgeInsets.all(10),
+                                  width: 280,
+
+                                  // Exercise
+                                  child: Center(
+                                    child: AppRegularText(
+                                        text: day['muscles'][muscleIndex]
+                                                ['exercises'][exerciseIndex]
+                                            ['exercise'],
+                                        color: Colors.white,
+                                        size: 22),
+                                  ),
+                                ),
+                                Positioned(
+                                  top: 10,
+                                  right: 10,
+                                  child: Container(
+                                    padding: const EdgeInsets.all(5),
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(20),
+                                        color: Colors.orange),
+                                    child: AppRegularText(
+                                        text:
+                                            '${day['muscles'][muscleIndex]['exercises'][exerciseIndex]['sets']} x ${day['muscles'][muscleIndex]['exercises'][exerciseIndex]['reps']} reps',
+                                        color: Colors.black,
+                                        size: 13),
+                                  ),
+                                ),
+                                Positioned(
+                                  bottom: 10,
+                                  right: 10,
+                                  child: Container(
+                                    padding: const EdgeInsets.all(5),
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(20),
+                                        color: Colors.orange),
+                                    child: Row(
+                                      children: [
+                                        const Icon(Icons.watch),
+                                        const SizedBox(width: 5),
+                                        AppRegularText(
+                                            text:
+                                                '${day['muscles'][muscleIndex]['exercises'][exerciseIndex]['rest']} rest',
+                                            color: Colors.black,
+                                            size: 13),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        }),
+                  ),
+                ],
+              ),
+            );
+          }
         },
       ),
     );
